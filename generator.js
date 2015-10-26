5bcbd92581e5deb68b1707526e5213d372b8b70a
josemam <josemam@users.noreply.github.com>
2015-10-26 19:21:35 +0100

    Límite de un verbo y concordancia
    Evita juntar dos expresiones con verbos (por ejemplo, "Podemos sí se puede" o "Venceremos es posible") y respeta la concordancia del castellano en las expresiones si la primera implica un género determinado (@ se cambiará por o si el género requerido es masculino o por a si es femenino, mientras que _ desaparecerá si debe ser masculino o se cambiará por a si debe ser femenino). Lo primero se aplicaría a los partidos de izquiedas y lo segundo a los de derechas, aunque podría extenderse a ambos si se amplía el JSON. Requiere añadir los sufijos de género pertinentes al JSON.

--- generator.js
+++ generator.js
@@ -1,4 +1,5 @@
 var word1, word2;
+var sufijos_femeninos, sufijos_verbos;
 var lado;
 // Ñapa, debería tener en cuenta el JSON definido...
 if ( !location.search || location.search.match(/izq/) ){
@@ -11,6 +12,8 @@ $(document).ready(function() {
     $.getJSON("words.json", function( data ) {
 	word1 = data[lado][0];
 	word2 = data[lado][1];
+	sufijos_femeninos = data["sufijos_femeninos"];
+	sufijos_verbos = data["sufijos_verbos"];
     });
 
     $("#generate").click(function(){
@@ -33,12 +36,30 @@ $(document).ready(function() {
     });
 });
 
+function test_sufijos(word, sufijos) {
+	for (var s in sufijos)
+		if (word.substring(word.length-sufijos[s].length) == sufijos[s])
+			return true;
+	
+	return false;
+}
+
+ function femenino(word) {
+	return test_sufijos(word, sufijos_femeninos);
+ }
  
+ function verbo(word) {
+	return test_sufijos(word, sufijos_verbos);
+ }
  
 function generate(){
-    var firstWord = word1[Math.floor(Math.random()*word1.length)];
-    var secondWord = word2[Math.floor(Math.random()*word2.length)];
-    
+	var firstWord = word1[Math.floor(Math.random()*word1.length)];
+	var primera_verbo = verbo(firstWord);
+	var primera_femenino = femenino(firstWord);
+	do {
+		var secondWord = word2[Math.floor(Math.random()*word2.length)].replace(/@/g, (primera_femenino ? "a" : "o")).replace(/_/g, (primera_femenino ? "a" : ""));
+	} while (primera_verbo && verbo(secondWord));
+		
     var partido = firstWord + " " + secondWord;
     
     document.getElementById("partido").innerHTML = partido;
